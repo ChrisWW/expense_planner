6e2619daaa584a8b56b731bd5c3bfa34bb39868a
Krystian Wiewior <krystian.wiewior@gmail.com>
2021-10-05 11:28:17 +0200

    more styling

--- lib/main.dart
+++ lib/main.dart
@@ -75,12 +75,30 @@ class MyHomePage extends StatelessWidget {
                       padding: EdgeInsets.all(10),
                       child: Text(
                         tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
                       ),
                     ),
-                    Column(children: <Widget>[
-                      Text(tx.title),
-                      Text(tx.date.toString()),
-                    ]),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               );
